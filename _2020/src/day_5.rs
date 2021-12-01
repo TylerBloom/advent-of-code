410fd11e1851cce0659e2df940e8f9a478f982be
TylerBloom <tylerbloom2222@gmail.com>
2021-12-01 01:09:58 -0500

    Solved 2020 problem 5 part 2

--- _2020/src/day_5.rs
+++ _2020/src/day_5.rs
@@ -1,57 +1,54 @@
 pub use crate::solution::Solution;
 
-#[derive(Debug)]
-struct BoardPass {
-    row: String,
-    col: String,
-}
-
-impl BoardPass {
-    fn get_id(&self) -> u64 {
-        self.row
+fn get_id(row: String, col: String) -> u64 {
+    row.chars()
+        .rev()
+        .enumerate()
+        .map(|(i, c)| if c == 'B' { 1_u64 << i } else { 0 })
+        .reduce(|a, b| a + b)
+        .unwrap()
+        * 8
+        + col
             .chars()
             .rev()
             .enumerate()
-            .map(|(i, c)| if c == 'B' { 1_u64 << i } else { 0 })
+            .map(|(i, c)| if c == 'R' { 1_u64 << i } else { 0 })
             .reduce(|a, b| a + b)
             .unwrap()
-            * 8
-            + self
-                .col
-                .chars()
-                .rev()
-                .enumerate()
-                .map(|(i, c)| if c == 'R' { 1_u64 << i } else { 0 })
-                .reduce(|a, b| a + b)
-                .unwrap()
-    }
 }
 
 pub struct Day5 {
-    data: Vec<BoardPass>,
+    data: Vec<u64>,
 }
 
 impl Solution<u64> for Day5 {
     fn parse_input(input: String) -> Self {
         let parsed: Vec<&str> = input.split("\n").collect();
-        let mut data: Vec<BoardPass> = Vec::with_capacity(parsed.len());
+        let mut data: Vec<u64> = Vec::with_capacity(parsed.len());
         for val in parsed {
             if !val.is_empty() {
-                data.push(BoardPass {
-                    row: val[..7].to_string(),
-                    col: val[7..].to_string(),
-                });
+                data.push(get_id(val[..7].to_string(), val[7..].to_string()));
             }
         }
+        data.sort();
         Day5 { data }
     }
 
     fn solve_part_one(&self) -> u64 {
-        self.data.iter().map( |b| b.get_id() ).max().unwrap()
+        *self.data.last().unwrap()
     }
 
     fn solve_part_two(&self) -> u64 {
-        0
+        let mut digest: u64 = 0;
+        for (i, val) in self.data[1..].iter().enumerate() {
+            // We use data[i] as the enumeration starts AFTER we take a slice,
+            // mapping index 1 in data to 0
+            if val - self.data[i] == 2 {
+                digest = val + 1;
+                break;
+            }
+        }
+        digest
     }
 }
 
@@ -65,12 +62,5 @@ mod tests {
         let input = String::from("BFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL");
         let solver = Day5::parse_input(input);
         assert_eq!(solver.solve_part_one(), 820);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let input = String::from("BFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL");
-        let solver = Day5::parse_input(input);
-        assert_eq!(solver.solve_part_two(), 0);
     }
 }
