4a74f4d5fe42d347cb2c824935019b0f9a4d9b90
TylerBloom <tylerbloom2222@gmail.com>
2025-12-05 12:26:06 -0500

    [2025] Solved day 5 problem B

--- _2025/src/day_five.rs
+++ _2025/src/day_five.rs
@@ -30,8 +30,65 @@ fn problem_a(input: &str) -> usize {
         .count()
 }
 
-fn problem_b(_input: &str) -> usize {
-    todo!()
+fn problem_b(input: &str) -> usize {
+    let (ranges, _) = input.split_once("\n\n").unwrap();
+    let mut ranges: Vec<_> = ranges
+        .lines()
+        .map(|line| line.split_once('-').unwrap())
+        .map(|(start, end)| start.parse::<usize>().unwrap()..=end.parse().unwrap())
+        .collect();
+
+    let mut buffer = Vec::with_capacity(ranges.len());
+
+    // We pop ranges off and compare that range to all other ranges, reducing the popped range
+    // until it is null or disjoint. Then, that range is placed into a new buffer.
+    //
+    // Once reduced, the popped ranges do not need to be checked against as they are disjount for
+    // all other ranges and newly popped-ranges are only ever reduced, not expanded.
+    while let Some(range) = ranges.pop() {
+        let (mut start, mut end) = range.into_inner();
+        for range in ranges.iter_mut() {
+            match (range.contains(&start), range.contains(&end)) {
+                // There is some overlap, which we need to address
+                (true, false) => {
+                    start = *range.end() + 1;
+                }
+                (false, true) => {
+                    end = *range.start() - 1;
+                }
+                // The new range is fully enveloped by the other range. Discard it
+                (true, true) => {
+                    start = usize::MAX;
+                    end = usize::MIN;
+                    break;
+                }
+                // The new range is either disjoint or envelopes the old range. No action needed
+                // for the disjoint case. In the other, we swap the enveloped range with the popped
+                // range and break. The swap puts us in the prior scenario. This is safe because we
+                // know that all popped ranges and the checked ranges in this loop are disjoint
+                // from the enveloping range. Thus, the enveloped range must also be disjoint from
+                // them.
+                (false, false) => {
+                    let r = start..=end;
+                    if r.contains(range.start()) && r.contains(range.end()) {
+                        *range = r;
+                        start = usize::MAX;
+                        end = usize::MIN;
+                        break;
+                    }
+                }
+            }
+        }
+        if start <= end {
+            buffer.push(start..=end);
+        }
+    }
+
+    buffer
+        .into_iter()
+        // Range::count doesn't iterate through all values
+        .map(Iterator::count)
+        .sum()
 }
 
 #[cfg(test)]
@@ -57,6 +114,6 @@ mod tests {
 
     #[test]
     fn test_problem_b_example() {
-        assert_eq!(problem_b(INPUT), 4174379265)
+        assert_eq!(problem_b(INPUT), 14)
     }
 }
